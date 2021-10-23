bfa3cdd00b2a22141e91188de23e945756a72d02
Patrick Deutschmann <patrick.deutschmann@student.tugraz.at>
2021-10-23 11:44:37 +0200

    fix: Make regression work with new model input

--- src/model/regression.py
+++ src/model/regression.py
@@ -1,8 +1,10 @@
+from typing import List
 from torch import nn
 import torch
 from torchmetrics.collections import MetricCollection
 from torchmetrics import MeanSquaredError, MeanAbsoluteError, R2Score
 
+from src.data.movement import ModelOutput
 from src.model.base import MovementPredictor
 
 
@@ -12,15 +14,13 @@ class RegressionMovementPredictor(MovementPredictor):
 
     def setup_metrics(self) -> MetricCollection:
         return MetricCollection(
-            {
-                "mse": MeanSquaredError(),
-                "mae": MeanAbsoluteError(),
-                "r2": R2Score()
-            }
+            {"mse": MeanSquaredError(), "mae": MeanAbsoluteError(), "r2": R2Score()}
         )
 
-    def preprocess_targets(self, target: torch.Tensor) -> torch.Tensor:
-        return target
+    def preprocess_targets(self, target: List[ModelOutput]) -> torch.Tensor:
+        return torch.stack(
+            [torch.tensor(x.price_movement, device=self.device) for x in target]
+        ).float()
 
     def postprocess_targets(self, target: torch.Tensor) -> torch.Tensor:
         return target
